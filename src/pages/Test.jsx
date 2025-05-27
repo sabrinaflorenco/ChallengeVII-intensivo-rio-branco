33c796a927cae9a364e7ab4f574024924e869e6e
Bernardo <bemedeiros66@gmail.com>
2025-05-27 19:44:09 -0300

    test-main conflito

---
+++ src/pages/Test.jsx
@@ -0,0 +1,14 @@
+export default function Test() {
+    return(
+        <div className="grid grid-cols-4 gap-4 h-9/12 w-9/12">
+            <div className="h-30 w-30 rounded-full bg-red-500"></div>
+            <div className="h-30 w-30 rounded-full bg-amber-500"></div>
+            <div className="h-30 w-30 rounded-full bg-red-900"></div>
+            <div className="h-30 w-30 rounded-full bg-purple-900"></div>
+            <div className="h-30 w-30 rounded-full bg-purple-500"></div>
+            <div className="h-30 w-30 rounded-full bg-green-500"></div>
+            <div className="h-30 w-30 rounded-full bg-red-900"></div>
+            <div className="h-30 w-30 rounded-full bg-purple-900"></div>
+        </div>
+    )
+}
