5920005e86be71595046a5d8147d233439a80ac8
Bernardo <bemedeiros66@gmail.com>
2025-05-27 19:32:28 -0300

    test de conflito

---
+++ src/pages/Test.jsx
@@ -0,0 +1,11 @@
+
+export default function Test() {
+    return(
+        <div className="grid grid-cols-4 gap-4">
+            <div className="h-30 w-30 rounded-full bg-blue-500"></div>
+            <div className="h-30 w-30 rounded-full bg-green-500"></div>
+            <div className="h-30 w-30 rounded-full bg-red-900"></div>
+            <div className="h-30 w-30 rounded-full bg-purple-900"></div>
+        </div>
+    )
+}
