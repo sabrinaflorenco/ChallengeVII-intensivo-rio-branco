bc1bd2dcb1bee23a5d598dc21c54a4bba7ab412d
Luis Felipe <luisfelipedsa@gmail.com>
2025-07-08 19:35:27 -0300

    Testando e melhorando expenses, como responsividade

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,56 +1,54 @@
-import { useEffect, useState } from "react";
-import Header from "../components/Header";
+import { useEffect, useState } from 'react'
+import Header from '../components/Header'
 
 export default function Expenses() {
-  const [description, setDescription] = useState("");
-  const [value, setValue] = useState("");
-  const [expen, setExpen] = useState([]);
-
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+  const [expen, setExpen] = useState([])
 
   useEffect(() => {
-    const stored = localStorage.getItem("expenses");
+    const stored = localStorage.getItem('expenses')
     if (stored) {
-      setExpen(JSON.parse(stored));
+      setExpen(JSON.parse(stored))
     }
-  }, []);
+  }, [])
 
- 
   useEffect(() => {
-    localStorage.setItem("expenses", JSON.stringify(expen));
-  }, [expen]);
+    localStorage.setItem('expenses', JSON.stringify(expen))
+  }, [expen])
 
   return (
-    <div className="flex-1 overflow-auto relative z-10">
-      <Header title="Despesas" />
-      <div className="flex items-center justify-center h-full">
+    <div className='flex-1'>
+      <Header title='Despesas' />
+      <div className='flex justify-center h-full max-sm:max-w-76 mt-40'>
         {/* bg-red-500 */}
-        <div className=" h-[700px] w-[800px]">
-          <form action="" className="">
+        <div className=' max-h-[700px] w-[800px]'>
+          <form action='' className=''>
             {/* adicionar gasto */}
             {/* ------------------------------------------------------ */}
-            <div className="flex gap-8 justify-center w-full max-sm:flex-col max-sm:items-center ">
+            <div className='flex gap-8 justify-center w-full max-sm:flex-col max-sm:items-center '>
               <input
-                type="text"
-                placeholder="Descrição do gasto"
+                type='text'
+                placeholder='Descrição do gasto'
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
-                className="shadow-2xs shadow-black rounded-full text-center w-70 max-sm:h-12"
+                className='shadow-2xs shadow-black rounded-full text-center w-70 max-sm:h-12'
               />
               <input
-                type="number"
-                placeholder="Valor do gasto"
+                type='number'
+                placeholder='Valor do gasto'
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
-                className="shadow-2xs shadow-black rounded-full text-center w-70 max-sm:h-12"
+                className='shadow-2xs shadow-black rounded-full text-center w-70 max-sm:h-12'
               />
               <button
-                type="submit"
-                className="bg-blue-500 text-white rounded-full p-3 cursor-pointer"
+                type='submit'
+                className='bg-blue-500 text-white rounded-full p-3 cursor-pointer'
                 onClick={(e) => {
-                  e.preventDefault();
-                  setExpen([...expen, { description, value }]);
-                  setDescription("");
-                  setValue("");
+                  e.preventDefault()
+                  setExpen([...expen, { description, value }])
+                  setDescription('')
+                  setValue('')
                 }}
               >
                 Adicionar
@@ -60,26 +58,23 @@ export default function Expenses() {
             {/* lista de gastos */}
             {/* ------------------------------------------------------ */}
             {expen.map((item, index) => (
-              <div
-                key={index}
-                className="grid grid-cols-3 gap-4 mt-10 "
-              >
-                <div className="flex items-center justify-center ">
-                  <p className="font-bold uppercase max-w-[800px]  ">
+              <div key={index} className='grid grid-cols-3 gap-4 mt-10 '>
+                <div className='flex items-center justify-center '>
+                  <p className='font-bold uppercase max-w-[800px]  '>
                     {item.description}
                   </p>
                 </div>
-                <div className="flex items-center justify-center">
-                  <p className="font-bold uppercase max-w-[800px] text-center">
+                <div className='flex items-center justify-center'>
+                  <p className='font-bold uppercase max-w-[800px] text-center'>
                     R$ {item.value}
                   </p>
                 </div>
 
-                <div className="flex items-center justify-center">
+                <div className='flex items-center justify-center'>
                   <button
-                    className="bg-red-500 rounded-full p-3 text-white font-bold cursor-pointer"
+                    className='bg-red-500 rounded-full p-3 text-white font-bold cursor-pointer'
                     onClick={() => {
-                      setExpen(expen.filter((_, i) => i !== index));
+                      setExpen(expen.filter((_, i) => i !== index))
                     }}
                   >
                     Deletar
@@ -87,17 +82,17 @@ export default function Expenses() {
                 </div>
               </div>
             ))}
-           
+
             {/* ------------------------------------------------------ */}
-			<div className="flex items-center justify-center mt-10">
-            <h1 className="text-2xl font-bold">
-              Total: R${" "}
-              {expen.reduce((acc, curr) => acc + Number(curr.value), 0)}
-            </h1>
-			</div>
+            <div className='flex items-center justify-center mt-10'>
+              <h1 className='text-2xl font-bold'>
+                Total: R${' '}
+                {expen.reduce((acc, curr) => acc + Number(curr.value), 0)}
+              </h1>
+            </div>
           </form>
         </div>
       </div>
     </div>
-  );
+  )
 }
